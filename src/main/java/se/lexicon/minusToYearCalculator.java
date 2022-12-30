af4b84de141c884ce60c5ccce63cd13ae490fedb
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-12-30 11:38:46 +0100

    Exercise

---
+++ src/main/java/se/lexicon/minusToYearCalculator.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+
+public class minusToYearCalculator {
+
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int totalYears = (int) (minutes / (365 * 24 * 60));
+            int totalDays = (int) (minutes % (365 * 24 * 60)) / (24 * 60);
+            System.out.println(minutes + " min = " + totalYears + " y and " + totalDays + " d");
+        }
+
+    }
+}
